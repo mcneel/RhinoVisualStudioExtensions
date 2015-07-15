079b36f955e276214443b89a1709d5eec7b9c33a
Dan Belcher <dan@mcneel.com>
2015-07-15 12:22:37 -0700

    Renaming the AddIn
    The GitHub repo has also been renamed

--- Properties/AddinInfo.cs
+++ Properties/AddinInfo.cs
@@ -6,8 +6,8 @@ using Mono.Addins;
 	Version = "5.9.4.0"
 )]
 
-[assembly:AddinName("Rhino Xamarin Studio Addin")]
+[assembly:AddinName("RhinoCommon Plugin Support")]
 [assembly:AddinCategory("Debugging")]
 [assembly:AddinDescription("Rhino plugin templates and debugger")]
 [assembly:AddinAuthor("Robert McNeel and Associates")]
-[assembly:AddinUrl("https://github.com/mcneel/RhinoMonodevelopAddin")]
+[assembly:AddinUrl("https://github.com/mcneel/RhinoCommonXamarinStudioAddin")]
