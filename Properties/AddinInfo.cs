7879e3b2e6c3d29147a7151e7edff8821eba5f8b
Curtis Wensley <curtis.wensley@gmail.com>
2016-04-05 10:39:56 -0700

    Update version to 5.10.3

--- Properties/AddinInfo.cs
+++ Properties/AddinInfo.cs
@@ -9,7 +9,7 @@ using Mono.Addins;
 // the same stable release of Xamarin Studio.
 [assembly:Addin(
   "RhinoXamarinStudioAddIn",
-	Version = "5.10.2.0"
+	Version = "5.10.3.0"
 )]
 
 // This controls the displayed name in the Xamarin Studio Add-In Manager...
