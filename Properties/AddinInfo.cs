bed8af8ed412ac59def5ed5f7f68b322fe5cef21
Curtis Wensley <curtis.wensley@gmail.com>
2017-08-22 15:27:15 -0700

    Bump version to 7.1

--- Properties/AddinInfo.cs
+++ Properties/AddinInfo.cs
@@ -9,7 +9,7 @@ using Mono.Addins;
 // the same stable release of Xamarin Studio.
 [assembly:Addin(
   "RhinoXamarinStudioAddIn",
-	Version = "7.0.0.0"
+	Version = "7.1.0.0"
 )]
 
 // This controls the displayed name in the Xamarin Studio Add-In Manager...
