3ba10e06681fcf5ad4e5d0eab59b9d5fc0bb8d97
Curtis Wensley <curtis.wensley@gmail.com>
2023-06-19 14:24:43 -0700

    Update version to 8.0

--- Rhino.VisualStudio.Mac/Properties/AddinInfo.cs
+++ Rhino.VisualStudio.Mac/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins;
 [assembly: Addin(
     "Mac",
     Namespace = "Rhino.VisualStudio",
-    Version = "7.28.0.0"
+    Version = "8.0.0.0"
 )]
 
 // This controls the displayed name in the Xamarin Studio Add-In Manager...
