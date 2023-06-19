a364dff9fbad3e13ffaf17d443cdcd149f4067c7
Curtis Wensley <curtis.wensley@gmail.com>
2023-06-19 13:39:41 -0700

    Add C++ Command
    - Fix up C++ GUID replacements (no whitespace!)
    - Fold C++ templates into same template ID's with language specifier
    - Add old-style C++ command template for VS as C++ projects can't use dotnet template engine

---
+++ Rhino.VisualStudio.Windows/Wizard/LegacyTemplateWizard.cs
@@ -0,0 +1,49 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using stdole;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rhino.VisualStudio.Windows.Wizard
+{
+    public class LegacyTemplateWizard : IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            if (replacementsDictionary.TryGetValue("$safeitemname$", out var fileinputname))
+            {
+                replacementsDictionary["$safefileinputname$"] = fileinputname;
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (!replacementsDictionary.TryGetValue($"$guid{i}$", out var guidString))
+                    continue;
+
+                var guid = Guid.Parse(guidString);
+                replacementsDictionary[$"$guid{i}x$"] = guid.ToString("X");
+            }
+        }
+
+        public bool ShouldAddProjectItem(string filePath) => true;
+    }
+}
--- Rhino.VisualStudio/CppRhinoPluginOptionsViewModel.cs
+++ Rhino.VisualStudio/CppRhinoPluginOptionsViewModel.cs
@@ -1,10 +1,7 @@
-﻿using System.IO;
-using System.Runtime.CompilerServices;
-using Microsoft.Win32;
-
-namespace Rhino.VisualStudio
+﻿namespace Rhino.VisualStudio
 {
-    public class CppRhinoPluginOptionsViewModel : BaseRhinoOptionsViewModel
+
+    public class CppRhinoPluginOptionsViewModel : BaseCppRhinoOptionsViewModel
     {
         public override string ProjectTitle => "New Rhino C++ Plug-In";
 
@@ -77,39 +74,6 @@ namespace Rhino.VisualStudio
             Host.SetParameter("Automation", UseAutomation.ToString());
             Host.SetParameter("Sockets", UseSockets.ToString());
             Host.SetParameter("SDL", UseSDL.ToString());
-        }
-
-        public override bool IsRhinoVersionValid => base.IsRhinoVersionValid && IsSdkPathValid;
-        public override string RhinoVersionValidationText
-        {
-            get
-            {
-                var version = RhinoVersion;
-                var msg = version > 0
-                    ? $"The C++ SDK for Rhino {RhinoVersion} is not installed."
-                    : $"The Rhino C++ SDK is not installed.";
-                var basemsg = base.RhinoVersionValidationText;
-                if (basemsg != null) return string.Join("\n", basemsg, msg);
-                return msg;
-            }
-        }
-
-        public bool IsSdkPathValid => !string.IsNullOrEmpty(GetSdkPath(RhinoVersion));
-        public string GetSdkPath(int version)
-        {
-            var path = Registry.GetValue($@"HKEY_LOCAL_MACHINE\SOFTWARE\McNeel\Rhinoceros\SDK\{version}.0", "InstallPath", null) as string;
-            if (path != null && Directory.Exists(path))
-                return path;
-            return null;
-        }
-
-        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            base.OnPropertyChanged(propertyName);
-            if (propertyName == nameof(RhinoVersion))
-            {
-                OnPropertyChanged(nameof(IsSdkPathValid));
-            }
         }
 
     }
