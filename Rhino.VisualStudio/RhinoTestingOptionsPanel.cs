0a3df19119a67c36e3f8db9f819e45fa62905ca6
Callum Sykes <28632338+clicketyclackety@users.noreply.github.com>
2025-02-21 13:03:12 +0100

    Created the Test Wizard

---
+++ Rhino.VisualStudio/RhinoTestingOptionsPanel.cs
@@ -0,0 +1,65 @@
+﻿using Eto.Forms;
+using Eto.Drawing;
+using System;
+using Rhino.VisualStudio.Controls;
+
+namespace Rhino.VisualStudio
+{
+    public class RhinoTestingOptionsPanel : BaseRhinoPageView
+    {
+        public RhinoTestingOptionsPanel(bool showProjectName)
+        {
+            var padding = new Padding(8);
+
+            InitLayout(showProjectName);
+
+            var information = new DynamicLayout();
+            information.AddSpace();
+            AddRhinoDownloadInfo(information);
+            information.AddSpace();
+
+            Information = information;
+        }
+
+        private void InitLayout(bool showProjectName)
+        {
+            var projectClassNameTextBox = new TextBox();
+            projectClassNameTextBox.TextBinding.BindDataContext((RhinoTestingOptionsViewModel m) => m.ProjectClassName);
+
+            var testClassNameTextBox = new TextBox();
+            testClassNameTextBox.TextBinding.BindDataContext((RhinoTestingOptionsViewModel m) => m.TestClassName);
+
+            var provideCommandSampleCheckBox = new CheckBox { Text = "Provide test sample", ToolTip = "Check to provide a sample test implementation" };
+            provideCommandSampleCheckBox.CheckedBinding.BindDataContext((RhinoTestingOptionsViewModel m) => m.IncludeSample);
+            provideCommandSampleCheckBox.BindDataContext(c => c.Enabled, (RhinoTestingOptionsViewModel m) => m.CanProvideSample);
+
+            Styles.Add<GroupBox>(null, g => g.Padding = this.Padding);
+
+            // layout
+            var layout = new DynamicLayout { DefaultSpacing = DefaultSpacing, Padding = this.Padding };
+
+            // top
+            layout.BeginVertical();
+            
+            if (showProjectName)
+                AddProjectName(layout);
+
+            layout.AddRow("Project class name", projectClassNameTextBox);
+            layout.AddRow("Test class name", testClassNameTextBox);
+            layout.EndVertical();
+
+            layout.BeginVertical();
+            layout.Add(new PanelSeparator("Options"));
+
+            AddRhinoVersion(layout);
+
+            AddIncludeVSCode(layout);
+
+            layout.Add(provideCommandSampleCheckBox);
+            layout.EndVertical();
+
+            Content = layout;
+        }
+
+    }
+}
