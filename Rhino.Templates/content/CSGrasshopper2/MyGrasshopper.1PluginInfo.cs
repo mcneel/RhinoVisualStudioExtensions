b5c71bfbaf1af1abdd11d9e84d9dec6f034a1fc0
Callum Sykes <callumsykes@protonmail.com>
2026-09-23 14:17:07 -0700

    Updated the GH2 template to Rhino 9

--- Rhino.Templates/content/CSGrasshopper2/MyGrasshopper.1PluginInfo.cs
+++ Rhino.Templates/content/CSGrasshopper2/MyGrasshopper.1PluginInfo.cs
@@ -10,20 +10,13 @@ namespace MyGrasshopper._1
     static T GetAttribute<T>() where T : Attribute => typeof(MyGrasshopper__1PluginInfo).Assembly.GetCustomAttribute<T>();
     
     public MyGrasshopper__1PluginInfo()
-      : base(new Guid("cd826b9b-8dbe-4c31-aac1-6fc7ea2bcfb7"),
-             new Nomen(
-                GetAttribute<AssemblyTitleAttribute>()?.Title, 
-                GetAttribute<AssemblyDescriptionAttribute>()?.Description),
-             typeof(MyGrasshopper__1PluginInfo).Assembly.GetName().Version)
-    { 
+    {
       Icon = AbstractIcon.FromResource("MyGrasshopper.1Plugin", typeof(MyGrasshopper__1PluginInfo));
     }
-    
+
     public override string Author => GetAttribute<AssemblyCompanyAttribute>()?.Company;
 
     public override sealed IIcon Icon { get; }
-
-    public override sealed string Copyright => GetAttribute<AssemblyCopyrightAttribute>()?.Copyright ?? base.Copyright;
 
     // public override sealed string Website => "https://mywebsite.example.com";
 
